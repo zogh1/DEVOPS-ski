5a83c14704274d9c80655c48b1c1f17967a3826e
Zoghlami Wassim <zoghlami.wassim@esprit.tn>
2023-11-12 16:57:26 +0100

    Mockito 1.0

---
+++ src/test/java/tn/esprit/SkiStationProject/InstructorTestMockito.java
@@ -0,0 +1,95 @@
+package tn.esprit.SkiStationProject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import tn.esprit.SkiStationProject.repositories.InstructorRepository;
+import tn.esprit.SkiStationProject.services.InstructorServicesImpl;
+import tn.esprit.SkiStationProject.entities.Instructor;
+import tn.esprit.SkiStationProject.repositories.InstructorRepository;
+
+
+public class InstructorTestMockito {
+
+    @InjectMocks
+    InstructorServicesImpl InstructorServices;
+
+    @Mock
+    InstructorRepository InstructorRepository;
+
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllInstructors() {
+        List<Instructor> Instructors = new ArrayList<>();
+        // Add test data to the list
+
+        when(InstructorRepository.findAll()).thenReturn(Instructors);
+
+        List<Instructor> result = InstructorServices.retrieveAllInstructors();
+
+        assertEquals(Instructors, result);
+    }
+
+    @Test
+    public void testAddInstructor() {
+        Instructor Instructor = new Instructor();
+        // Set properties for the test activity sector
+
+        when(InstructorRepository.save(Instructor)).thenReturn(Instructor);
+
+        Instructor result = InstructorServices.addInstructor(Instructor);
+
+        assertEquals(Instructor, result);
+    }
+
+    @Test
+    public void testRetriveInstructor() {
+        Long idToret = 1L;
+
+        InstructorServices.retrieveInstructor(idToret);
+
+        verify(InstructorRepository).findById(idToret);
+    }
+
+    @Test
+    public void testUpdateInstructor() {
+        Instructor Instructor = new Instructor();
+        // Set properties for the test activity sector
+
+        when(InstructorRepository.save(Instructor)).thenReturn(Instructor);
+
+        Instructor result = InstructorServices.updateInstructor(Instructor);
+
+        assertEquals(Instructor, result);
+    }
+
+    @Test
+    public void testRetrieveInstructor() {
+        Long idToRetrieve = 1L;
+        Instructor Instructor = new Instructor();
+        // Set properties for the test activity sector
+
+        when(InstructorRepository.findById(idToRetrieve)).thenReturn(Optional.of(Instructor));
+
+        Instructor result = InstructorServices.retrieveInstructor(idToRetrieve);
+
+        assertEquals(Instructor, result);
+    }
+
+}
